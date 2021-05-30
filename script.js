1201a9bcb92014401b77f00c4c367cf4ba91d876
Harun Al Rasyid <harundarat@gmail.com>
2021-05-30 08:23:41 +0700

    add destructuring on script.js (#1)
    * modify file.txt
    
    * add cetakNama function in script.js
    
    * add destructuring object in script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const name = {
   firstName: 'harun',
   middleName: 'al',
-  lastName: 'rasyid
+  lastName: 'rasyid'
 }
 
 console.info(`${firstName} ${middleName} ${lastName}`)
+
+function cetakNama(firstName, middleName, lastName) {
+	console.info(`${firstName} ${middleName} ${lastName}`)
+}
+
+cetakNama('harun','al','rasyid')
+
+const {firstname,middleName,lastName} = name
